d84385c85b6125608ee6aa6bc77f049c44d72bd0
itforget <itforget11@gmail.com>
2023-12-22 22:18:45 -0300

    change gitignore

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"x6JO9KnS49pJDPny9/vTUZ2V7+DEYSrYYBTzGuX+66c=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"/igKBeAP3kH0abL4u3Y4exxs16sjVe+GahAkvi57hD8=\"\n}"
